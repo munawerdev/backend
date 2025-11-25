95d525d4a135c42255b755d40798e9d485c4a1b5
Munawer <117267127+Munawer2022@users.noreply.github.com>
2025-11-25 23:55:53 +0500

    feat: Set up initial Express application with user registration route and controller.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-export { app }
+
+//routes import
+import userRoutes from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRoutes);
+
+export { app };
