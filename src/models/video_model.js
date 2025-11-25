4ba9e08535e1fae76daf58c5a0a580b874a0630a
Munawer <117267127+Munawer2022@users.noreply.github.com>
2025-11-25 19:58:28 +0500

    feat: Enhance authentication and video management features by adding JWT support, bcrypt for password hashing, and a new video model

---
+++ src/models/video_model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    views: { type: Number, default: 0 },
+    isPublished: { type: Boolean, default: true },
+    owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    videoUrl: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    comments: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
